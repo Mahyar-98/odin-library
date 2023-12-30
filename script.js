ff87899ed19a7fbabeff3842dfad8197b7cfa188
Mahyar <mahyar.erfanian1998@gmail.com>
2023-12-30 11:17:10 -0500

    write a simple code to add the books in the library to the html

--- script.js
+++ script.js
@@ -8,13 +8,26 @@ function Book(title, author, pages, read) {
     this.info = () => {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "was read already" : "not read yet"}.`
     }
-    addBookToLibrary(book)
+    addBookToLibrary(this)
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
-console.log(theHobbit)
-console.log(Book.prototype)
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
+const meBeforeYou = new Book("Me Before You", "Jojo Moyes", 385, true)
+const prideAndPrejudice = new Book("Pride and Prejudice", "Jane Austen", 496, true)
+
+/* ---------------------------------------------------------------------------------- */
+
+const page = document.getElementsByTagName("p")
+const newBookBtn = document.getElementById("newBookBtn")
+newBookBtn.addEventListener("click", () => {document.getElementById("p").textContent = "hello!"})
+const bookList = document.createElement("ul")
+document.body.append(bookList)
+for (let i of myLibrary) {
+    book = document.createElement("li")
+    book.textContent = i.title
+    bookList.appendChild(book)
+}
