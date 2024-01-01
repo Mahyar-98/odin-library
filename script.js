7a0c18ed5482e5cb5b1f5b231e12fa1d8ff008ef
Mahyar <mahyar.erfanian1998@gmail.com>
2024-01-01 12:40:06 -0500

    Created form input fields without label

--- script.js
+++ script.js
@@ -31,3 +31,35 @@ for (let i of myLibrary) {
     book.textContent = i.title
     bookList.appendChild(book)
 }
+const form = document.createElement("form")
+const inputTitle = document.createElement("input")
+inputTitle.type = "text"
+inputTitle.name = "title"
+inputTitle.placeholder = "Enter the title of the book here"
+
+const inputAuthor = document.createElement("input")
+inputAuthor.type = "text"
+inputAuthor.name = "author"
+inputAuthor.placeholder = "What's the name of the author?"
+
+const inputPages = document.createElement("input")
+inputPages.type = "integer"
+inputPages.name = "pages"
+inputPages.placeholder = "How many pages does this book have?"
+
+const inputRead = document.createElement("input")
+inputRead.type = "checkbox"
+inputRead.name = "read"
+inputRead.checked = false
+
+const submitBtn = document.createElement("input")
+submitBtn.type = "submit"
+submitBtn.value = "Submit"
+
+form.appendChild(inputTitle)
+form.appendChild(inputAuthor)
+form.appendChild(inputPages)
+form.appendChild(inputRead)
+form.appendChild(submitBtn)
+
+document.body.appendChild(form)
