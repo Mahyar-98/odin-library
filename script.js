7f3788ea0c053de82aa5010269a4b17866bd49b3
Mahyar <mahyar.erfanian1998@gmail.com>
2024-01-03 12:47:14 -0500

    Add the labels and break elements

--- script.js
+++ script.js
@@ -31,22 +31,28 @@ for (let i of myLibrary) {
     book.textContent = i.title
     bookList.appendChild(book)
 }
+
 const form = document.createElement("form")
+
+const labelTitle = createLabel("Title: ", "title")
 const inputTitle = document.createElement("input")
 inputTitle.type = "text"
 inputTitle.name = "title"
 inputTitle.placeholder = "Enter the title of the book here"
 
+const labelAuthor = createLabel("Author: ", "author")
 const inputAuthor = document.createElement("input")
 inputAuthor.type = "text"
 inputAuthor.name = "author"
 inputAuthor.placeholder = "What's the name of the author?"
 
+const labelPages = createLabel("Number of pages: ", "pages")
 const inputPages = document.createElement("input")
 inputPages.type = "integer"
 inputPages.name = "pages"
 inputPages.placeholder = "How many pages does this book have?"
 
+const labelRead = createLabel("Have you read the book? ", "read")
 const inputRead = document.createElement("input")
 inputRead.type = "checkbox"
 inputRead.name = "read"
@@ -56,10 +62,25 @@ const submitBtn = document.createElement("input")
 submitBtn.type = "submit"
 submitBtn.value = "Submit"
 
+form.appendChild(labelTitle)
 form.appendChild(inputTitle)
+form.appendChild(document.createElement('br'))
+form.appendChild(labelAuthor)
 form.appendChild(inputAuthor)
+form.appendChild(document.createElement('br'))
+form.appendChild(labelPages)
 form.appendChild(inputPages)
+form.appendChild(document.createElement('br'))
+form.appendChild(labelRead)
 form.appendChild(inputRead)
+form.appendChild(document.createElement('br'))
 form.appendChild(submitBtn)
 
 document.body.appendChild(form)
+
+function createLabel(text, forAttribute) {
+    const label = document.createElement('label');
+    label.textContent = text;
+    label.htmlFor = forAttribute;
+    return label
+}
